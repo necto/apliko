39dd8a3f36cfd72ad8f37b682a8e065610f4884f
Arseniy Zaostrovnykh <necto.not@gmail.com>
2013-07-03 17:40:43 +0400

    Authentication realm is switched to database, for easy managing from application

--- db.sql
+++ db.sql
@@ -1,7 +1,18 @@
 
--- add password autentification to joe in pg_hba.conf :
+-- add password autentification to joe, bids_admin, tomcat_auth in pg_hba.conf :
 -- sudo emacs /etc/postgresql/9.1/main/pg_hba.conf
 -- sudo service postgresql restart
+
+drop database bids_auth;
+drop database bids;
+
+drop user joe;
+drop user bids_admin;
+drop user tomcat_auth;
+
+---------------------------
+-- Requests database
+---------------------------
 
 create user joe with password 'lapass';
 create database bids;
@@ -14,4 +25,38 @@ create table claims (
        id integer primary key,
        name text
 );
+
+---------------------------
+-- Authentication base
+---------------------------
+
+set role postgres;
+
+create user tomcat_auth with password 'tomcat_188';
+create user bids_admin with password '11111';
+create database bids_auth;
+grant all privileges on database bids_auth to bids_admin;
+
+\c bids_auth;
+set role bids_admin;
+
+create table users (
+  user_name         varchar(15) not null primary key,
+  user_pass         varchar(15) not null
+);
+
+create table user_roles (
+  user_name         varchar(15) not null,
+  role_name         varchar(15) not null,
+  primary key (user_name, role_name)
+);
+
+insert into users values ('tom', 'aga');
+insert into users values ('jerry', 'jer');
+insert into user_roles values ('tom', 'manager-script');
+insert into user_roles values ('jerry', 'manager-gui');
+
+grant select on users to tomcat_auth;
+grant select on user_roles to tomcat_auth;
+
 \q
